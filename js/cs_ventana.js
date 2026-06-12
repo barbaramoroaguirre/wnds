8a3aadb6608708bc4945a44897d5ff11ef670cf5
barbara <barbaramoroaguirre@gmail.com>
2026-06-12 17:42:39 +0200

    versión básica

---
+++ js/cs_ventana.js
@@ -0,0 +1,82 @@
+// abrir ventana CV
+const ventanaCV = document.getElementById('ventana-cv-completa');
+const btnAbrirCV = document.getElementById('abrirVentanaCV');
+const btnCerrarCV = document.getElementById('cerrarVentana');
+const headerCV = document.getElementById('barra-ventana');
+
+// Mostrar la ventanaCV
+btnAbrirCV.addEventListener('click', () => {
+    ventanaCV.style.display = 'block';
+    // Centrar la ventanaCV al abrir
+    ventanaCV.style.left = (window.innerWidth - ventanaCV.offsetWidth) / 2 + 'px';
+    ventanaCV.style.top = (window.innerHeight - ventanaCV.offsetHeight) / 2 + 'px';
+});
+
+// Ocultar la ventanaCV
+btnCerrarCV.addEventListener('click', () => {
+    ventanaCV.style.display = 'none';
+});
+
+// Lógica para arrastrar la ventanaCV
+let isDragging = false;
+let offsetX, offsetY;
+
+
+
+headerCV.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    offsetX = e.clientX - ventanaCV.offsetLeft;
+    offsetY = e.clientY - ventanaCV.offsetTop;
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        ventanaCV.style.left = e.clientX - offsetX <= document.documentElement.clientWidth - ventanaCV.clientWidth - 10 ? (e.clientX - offsetX) + 'px' : ventanaCV.style.left;
+        ventanaCV.style.top = (e.clientY - offsetY) + 'px';
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+//Abrir carpeta UX
+const ventanaUX = document.getElementById('ventana-uxui-completa');
+const btnAbrirUX = document.getElementById('abrirVentanaUX');
+const btnCerrarUX = document.getElementById('cerrarVentanaUX');
+const headerUX = document.getElementById('barra-ventana');
+
+// Mostrar la ventanaUX
+btnAbrirUX.addEventListener('click', () => {
+    ventanaUX.style.display = 'block';
+    // Centrar la ventanaUX al abrir
+    ventanaUX.style.left = (window.innerWidth - ventanaUX.offsetWidth) / 2 + 'px';
+    ventanaUX.style.top = (window.innerHeight - ventanaUX.offsetHeight) / 2 + 'px';
+});
+
+// Ocultar la ventanaUX
+btnCerrarUX.addEventListener('click', () => {
+    ventanaUX.style.display = 'none';
+});
+
+
+// Lógica para arrastrar la ventanaUX
+let isDraggingUX = false;
+let offsetXUX, offsetYUX;
+
+headerUX.addEventListener('mousedown', (e) => {
+    isDraggingUX = true;
+    offsetXUX = e.clientX - ventanaUX.offsetLeft;
+    offsetYUX = e.clientY - ventanaUX.offsetTop;
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (isDraggingUX) {
+        ventanaUX.style.left = (e.clientX - offsetXUX) + 'px';
+        ventanaUX.style.top = (e.clientY - offsetYUX) + 'px';
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDraggingUX = false;
+});
---
+++ js/paint.js
@@ -0,0 +1,4 @@
+const canvas = document.querySelector("canvas")
+
+//para pedirle al canvas el contexto del dibujo 2d (pincel)
+const ctx = canvas.getContext("2d")
